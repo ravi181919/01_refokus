26dcaa78175cdb27016c524f589397bdc87e6339
ravigupta <rg187888@gmail.com>
2024-08-05 18:06:05 +0530

    gh pages done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import Navbar from "./components/Navbar";
 import Routing from "./router/Routing";
 import LocomotiveScroll from "locomotive-scroll";
 
-
 export default function App() {
   const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className='w-full bg-black text-white font-["Satoshi"]'>
       <Navbar />
       <Routing />
-      
     </div>
   );
 }
